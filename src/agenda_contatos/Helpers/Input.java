945073fdbd0e591ba9704398c90e5ff9ff4a9918
Washington Ferreira <35137108+Batcleta@users.noreply.github.com>
2022-12-13 08:04:49 -0300

    Update Input.java

--- src/agenda_contatos/Helpers/Input.java
+++ src/agenda_contatos/Helpers/Input.java
@@ -1,11 +1,23 @@
 package agenda_contatos.Helpers;
 
+import agenda_contatos.Helpers.Errors.NotNullable;
+
 import java.util.Scanner;
 
 public class Input {
-    Scanner input = new Scanner(System.in);
+    public static String string() {
+        return new Scanner(System.in).nextLine().trim();
+    }
 
-    public String string() {
-        return input.nextLine();
+    public static String stringNotNullable (String nomeDoCampo, int tentativas){
+       try {
+           System.out.printf("Por favor informe o %s", nomeDoCampo);
+           String input = new Scanner(System.in).nextLine().trim();
+
+           if(input.isBlank()) throw new NotNullable()
+       }catch (NotNullable ex){
+           System.out.printf("msg: %s, code: %s", ex.getMessage(), ex.getCode());
+       }
+
     }
 }
